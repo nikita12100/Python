b62baacefa3421ba2ad493780c605cb3b577930a
Nikita <33327392+nikita12100@users.noreply.github.com>
2018-03-27 16:40:44 +0300

    Add files via upload
    исправил путь и много еще чего
    27.03.2018

--- 1_course/view_1/generate.py
+++ 1_course/view_1/generate.py
@@ -9,8 +9,8 @@ import random
 def read_model(path):  # восстановили словарь
     pairs = {}
     with open(path, 'r') as f:
-        for i in f:
-            s = i.split()  # полчучаем пару
+        for line in f:
+            s = line.split()  # полчучаем пару
             if (s[0], s[1]) in pairs:  # если уже есть, то увеличиваем счетчик
                 pairs[s[0], s[1]] += 1
             else:
@@ -28,9 +28,8 @@ def build_sentence(pairs, seed):
         frequency = []  # следующие слова за seed
         for words in pairs:
             if words[0] == seed:  # нашли пару с seed-world[]
-                for k in range(pairs.get(words)):
-                    frequency.append(
-                        words[1])  # записываем в freq слова следующие за seed(солько , сколько вхождений пар)
+                for k in range(pairs.get(words)):  # <значиние> раз добавляем в freq <слово2>
+                    frequency.append(words[1])
         seed = random.choice(frequency)  # выбираем следующее
     return result
 
@@ -42,23 +41,20 @@ def write_result(path, result):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help='Путь к файлу, из которого загружается модель.')
+    parser.add_argument('--model', help='Путь к файлу, из которого загружается модель. Имя модели должно быть "model.txt" !')
     parser.add_argument('--seed', help='Начальное слово.')
     parser.add_argument('--length', help='Длина последовательности.')
     parser.add_argument('--output', help='Файл, в который будет записан результат.')
     args = parser.parse_args()
     # аргументы
 
-    pairs = read_model(args.model)
+    pairs = read_model(args.model + 'model.txt')
 
     seed = ''
-    keys = pairs.keys()  # выбираем слово с которого начнем посторение
     if args.seed is not None:
         seed = args.seed
     else:
-        for i in pairs:  # вытаскмваем первый ключ
-            seed = i[0]
-            break
+        seed = random.choice(list(pairs))[0]
 
     result = build_sentence(pairs, seed)
 
--- 1_course/view_1/train.py
+++ 1_course/view_1/train.py
@@ -1,56 +1,44 @@
 import argparse
 import re
-import fileinput  # это зачем
+import sys
+
+#запись модели в файл
+def write_model(words, out_path):
+    with open(out_path + 'model.txt', 'a') as wr:
+        for i in range(len(words) - 1):  # идем по всем словам , кроме поледнего, тк у него нет пары
+            wr.write(words[i] + ' ' + words[i + 1] + '\n')  # записавыем пары слов
 
 
 # создание модели
-# в модели храниятся пары слов
-# пары могут дублироватья
+# в модели храниятся пары слов, пары могут дублироватья
 # подсчет количества вхождений пар в "genereate"
-def make_model(in_path, low_case, out_path):  
-    with open(in_path, 'r') as rd, open(out_path, 'w') as wr:
-        for line in rd:  # читаем из файла
-            line = line.strip()  # убрали пробелы сначала и с конца  #  это не нужно, split() их уберет
-            reg = re.compile('[^a-zA-Zа-яА-Я ]')  # очистили от неалфавитных 
-            line = reg.sub('', line)  # и тут тоже
-            # line = re.sub('[^a-zA-Zа-яА-Я ]+', '', line) в одну строчку
-            if low_case == 1:  # приводим  к lowercase, если надо
-                line = line.lower()
+def make_model(in_path, low_case, out_path):
+    if args.input is not None:
+        f = open(args.input + 'input.txt', 'r')
+    else:
+        f = sys.stdin
 
-            words = line.split()  # множество пар
-            # прочитали из файла
+    last = '_'  # соединяем последнее слово с первым
+    for line in f:  # читаем из файла
+        line = re.sub('[^a-zA-Zа-яА-Я ]+', '', line) # очистили от неалфавитных символов
+        if low_case:  # приводим  к lowercase, если надо
+            line = line.lower()
 
-            for i in range(len(words) - 1):  # идем по всем словам , кроме поледнего, тк у него нет пары
-                wr.write(words[i] + ' ' + words[i + 1] + '\n')  # записавыем пары слов
-                
-                # а последнее куда дел? его тоже надо сохранить
+        words = line.split()  # множество пар
+
+        words.insert(0, last)
+        last = words[-1]
+        write_model(words, out_path)
+
+    f.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', help='Путь к директории, в которой лежит коллекция документов.') 
-    # давай внимательно посмотрим на то, что написано, и что ты делаешь
-    # нужно, чтобы команда принимала путь к ПАПКЕ в которой лежат файлы
-    
-    
-    
-    # а еще она не запускается
-    # mshekhter-osx:view_1 mshekhter$ python3 train.py --input Read.txt
-#     Traceback (most recent call last):
-#       File "train.py", line 35, in <module>
-#         words = make_model(args.input, args.lc, args.model)
-#       File "train.py", line 11, in make_model
-#         with open(in_path, 'r') as rd, open(out_path, 'w') as wr:
-#     TypeError: expected str, bytes or os.PathLike object, not NoneType
-
-
-
-    parser.add_argument('--model', help='Путь к файлу, в который сохраняется модель.')
+    parser.add_argument('--input', help='Путь к директории, в которой лежит коллекция документов.Файл должен называться "input.txt" !')
+    parser.add_argument('--model', help='Путь к файлу, в который сохраняется модель. Имя модели должно быть "model.txt" !')
     parser.add_argument('--lc', action='store_true', help='Приводить ли текст к нижнему ригистру ? (0 - нет, 1 - да)')
     args = parser.parse_args()
     # аргументы
 
-    if args.input is not None:
-        words = make_model(args.input, args.lc, args.model)
-    else:
-        words = make_model(fileinput.input(), args.lc, args.model)
+    words = make_model(args.input, args.lc, args.model)
